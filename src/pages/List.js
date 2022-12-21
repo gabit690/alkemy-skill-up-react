2ef6add899b669169b4df2759b2343c08a565233
gabit <gabit690@gmail.com>
2022-12-21 18:47:00 -0300

    Structure of Movie's list

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,32 +1,40 @@
-import { useEffect } from "react";
-import { Link, useNavigate, Navigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link, Navigate } from 'react-router-dom';
+import axios from "axios";
 
 function List() {
 
+  const [movies, setMovies] = useState([]);
   const token = localStorage.getItem('token');
-  const navigate = useNavigate();
 
-  
   useEffect(() => {
-    if (token == null) {
-      navigate('/');
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    const endpoint = "https://api.themoviedb.org/3/discover/movie?api_key=8d6e07733691f3c82fe2e5567d26ff11&language=en-US&page=1";
+    axios.get(endpoint)
+      .then(response => setMovies(response.data.results));
+
+  }, [setMovies]);
 
   return (
     <>
       {!token && <Navigate to="/" />}
       <div className="row">
-      <div class="card" style={{width: '18rem'}}>
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <Link to="#" class="btn btn-primary">Go somewhere</Link>
-        </div>
+      {
+        movies.map((movie, index) => {
+          return (
+            <div className="col-3 mt-3" key={index}>
+              <div class="card">
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">{ movie.title }</h5>
+                  <p class="card-text">{ movie.overview }</p>
+                  <Link to="#" class="btn btn-primary">Go somewhere</Link>
+                </div>
+              </div>
+            </div>
+          );
+        })
+      }
       </div>
-    </div>
     </>
 
   );
